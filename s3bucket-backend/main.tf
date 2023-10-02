2c41303803e65bae697481f91e1833349a71be7e
joseph okwudi <josephflo55@gmail.com>
2023-10-02 16:06:38 -0700

    Scope / Description with fix, feat (SemVer) Problem Solution Note

---
+++ s3bucket-backend/main.tf
@@ -0,0 +1,43 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "bootcamp32-$(env)-$(random.int)"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = var.enz.id
+  }
+}
+
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mybucket"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}   
+
+
+variable "region" {
+    type = string
+    default = "us-west-2"
+  
+}variable "env" {
+    type = string
+    default = "Dev"
+  
+}
+
+
+
